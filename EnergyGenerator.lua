9ee1fef2e3398786f6dfce899d6f87655d6e5647
Pepper0ni <pepper0ni3steps@gmail.com>
2022-02-21 23:18:35 +0000

    Initial Commit

---
+++ EnergyGenerator.lua
@@ -0,0 +1,390 @@
+energyTable={
+ {set="Base",
+  api=true,
+  code="base1",
+  types={
+   ["Grass"]={num="99"},
+   ["Fire"]={num="98"},
+   ["Water"]={num="102"},
+   ["Lightning"]={num="100"},
+   ["Psychic"]={num="101"},
+   ["Fighting"]={num="97"},
+  },
+  date="19990109"
+ },
+ {set="Expedition Base Set",
+  api=true,
+  code="ecard1",
+  types={
+   ["Grass"]={num="162"},
+   ["Fire"]={num="161"},
+   ["Water"]={num="165"},
+   ["Lightning"]={num="163"},
+   ["Psychic"]={num="164"},
+   ["Fighting"]={num="160"},
+  },
+  date="20020915"
+ },
+ {set="Ruby & Sapphire",
+  api=true,
+  code="ex1",
+  types={
+   ["Grass"]={num="104"},
+   ["Fire"]={num="108"},
+   ["Water"]={num="106"},
+   ["Lightning"]={num="109"},
+   ["Psychic"]={num="107"},
+   ["Fighting"]={num="105"},
+  },
+  date="20030701"
+ },
+ {set="Emerald",
+  api=true,
+  code="ex9",
+  types={
+   ["Grass"]={num="101"},
+   ["Fire"]={num="102"},
+   ["Water"]={num="103"},
+   ["Lightning"]={num="104"},
+   ["Psychic"]={num="105"},
+   ["Fighting"]={num="106"}
+  },
+  date="20050501"
+ },
+ {set="Holon Phantoms",
+  api=true,
+  code="ex13",
+  types={
+   ["Grass"]={num="105"},
+   ["Fire"]={num="106"},
+   ["Water"]={num="107"},
+   ["Lightning"]={num="108"},
+   ["Psychic"]={num="109"},
+   ["Fighting"]={num="110"}
+  },
+  date="20060501"
+ },
+ {set="Power Keepers",
+  api=true,
+  code="ex16",
+  types={
+   ["Grass"]={num="103"},
+   ["Fire"]={num="104"},
+   ["Water"]={num="105"},
+   ["Lightning"]={num="106"},
+   ["Psychic"]={num="107"},
+   ["Fighting"]={num="108"}
+  },
+  date="20070202"
+ },
+ {set="Diamond & Pearl",
+  api=true,
+  code="dp1",
+  types={
+   ["Grass"]={num="123"},
+   ["Fire"]={num="124"},
+   ["Water"]={num="125"},
+   ["Lightning"]={num="126"},
+   ["Psychic"]={num="127"},
+   ["Fighting"]={num="128"},
+   ["Darkness"]={num="129"},
+   ["Metal"]={num="130"}
+  },
+  date="20070501"
+ },
+ {set="Movie Commemoration Random Pack",
+  types={
+   ["Grass"]={steamUrl="1832405798625465500/D3730F4B293AB62AB619693A2CB66F775773DC7C/",num="E1"},
+   ["Fire"]={steamUrl="1832405798625455940/509F5D4C4FC7777ACD7C262255FD40E8AAD5462D/",num="E2"},
+   ["Water"]={steamUrl="1832405798625485038/0A15524996F5202AE08493ED09AE0AC47B6A5CD8/",num="E3"},
+   ["Lightning"]={steamUrl="1832405798625494298/F8DFA471580360CB70787FA0889BD65D7240FD69/",num="E4"},
+   ["Psychic"]={steamUrl="1832405798625513601/B00633C1C3D9963186F18792FD692591D2743132/",num="E5"},
+   ["Fighting"]={steamUrl="1832405798625522357/12D4FAB3EDA2E47C3A56A5F2A5D90FA54173E2DD/",num="E6"},
+   ["Darkness"]={steamUrl="1832405798625455574/3CAEED33508C134DFBF7DE1EBEAC9E00B534A639/",num="E7"},
+   ["Metal"]={steamUrl="1832405798625531534/F2EEA63644E26D0EDA05D8BA1ECF15952ADBF36D/",num="E8"}
+  },
+  date="20090718"
+ },
+ {set="HeartGold & SoulSilver",
+  api=true,
+  code="hgss1",
+  types={
+   ["Grass"]={num="115"},
+   ["Fire"]={num="116"},
+   ["Water"]={num="117"},
+   ["Lightning"]={num="118"},
+   ["Psychic"]={num="119"},
+   ["Fighting"]={num="120"},
+   ["Darkness"]={num="121"},
+   ["Metal"]={num="122"}
+  },
+  date="20100210"
+ },
+ {set="Black & White",
+  api=true,
+  code="bw1",
+  types={
+   ["Grass"]={num="105"},
+   ["Fire"]={num="106"},
+   ["Water"]={num="107"},
+   ["Lightning"]={num="108"},
+   ["Psychic"]={num="109"},
+   ["Fighting"]={num="110"},
+   ["Darkness"]={num="111"},
+   ["Metal"]={num="112"}
+  },
+  date="20110425"
+ },
+ {set="XY",
+  api=true,
+  code="xy1",
+  types={
+   ["Grass"]={num="132"},
+   ["Fire"]={num="133"},
+   ["Water"]={num="134"},
+   ["Lightning"]={num="135"},
+   ["Psychic"]={num="136"},
+   ["Fighting"]={num="137"},
+   ["Darkness"]={num="138"},
+   ["Metal"]={num="139"},
+   ["Fairy"]={num="140"}
+  },
+  date="20140205"
+ },
+ {set="Generations",
+  api=true,
+  code="g1",
+  types={
+   ["Grass"]={num="75"},
+   ["Fire"]={num="76"},
+   ["Water"]={num="77"},
+   ["Lightning"]={num="78"},
+   ["Psychic"]={num="79"},
+   ["Fighting"]={num="80"},
+   ["Darkness"]={num="81"},
+   ["Metal"]={num="82"},
+   ["Fairy"]={num="83"}
+  },
+  date="20160222"
+ },
+ {set="Premium Champion Pack",
+  types={
+   ["Grass"]={steamUrl="1832405798633758004/AF075BF8908D8D6A857AC76AB9802B1C08B2759A/",num="E1"},
+   ["Fire"]={steamUrl="1832405798633729448/21A3FBF06D609A68E1C003BDAA1F1699E0D2EAE3/",num="E2"},
+   ["Water"]={steamUrl="1832405798633768500/EC28E9A9B9685314A298C3B6B1A1B655ED5EF6DB/",num="E3"},
+   ["Lightning"]={steamUrl="1832405798633533636/94DE3319F2195AEFFB7F4532B67C3944AACF9F51/",num="E4"},
+   ["Psychic"]={steamUrl="1832405798633527707/8B202A12DFF1DEC22E8BF9F71A8055A35B40F243/",num="E5"},
+   ["Fighting"]={steamUrl="1832405798633707490/31C6F6317CA8C3E97FF71B4EF746DC7C323F395A/",num="E6"},
+   ["Darkness"]={steamUrl="1832405798633567716/B31D366D557C2E7956265574FF3F858325CC0F1B/",num="E7"},
+   ["Metal"]={steamUrl="1832405798633570117/5CDE07ECEA3B19C0B618F6834048F4A8958C4C8F/",num="E8"},
+   ["Fairy"]={steamUrl="1832405798633525549/BCEE1F11DA6ED59F7775123F8FA9E619341E398B/",num="E8"}
+  },
+  date="20160416"
+ },
+ {set="Evolutions",
+  api=true,
+  code="xy12",
+  types={
+   ["Grass"]={num="91"},
+   ["Fire"]={num="92"},
+   ["Water"]={num="93"},
+   ["Lightning"]={num="94"},
+   ["Psychic"]={num="95"},
+   ["Fighting"]={num="96"},
+   ["Darkness"]={num="97"},
+   ["Metal"]={num="98"},
+   ["Fairy"]={num="99"}
+  },
+  date="20161102"
+ },
+ {set="Sun & Moon",
+  api=true,
+  code="sm1",
+  types={
+   ["Grass"]={num="164"},
+   ["Fire"]={num="165"},
+   ["Water"]={num="166"},
+   ["Lightning"]={num="167"},
+   ["Psychic"]={num="168"},
+   ["Fighting"]={num="169"},
+   ["Darkness"]={num="170"},
+   ["Metal"]={num="171"},
+   ["Fairy"]={num="172"}
+  },
+  date="20170203"
+ },
+ {set="Gen 7 Secret",
+  api=true,
+  types={
+   ["Grass"]={code="sm2",setName="Guardians Rising",date="20170505",num="167"},
+   ["Fire"]={code="sm3",setName="Burning Shadows",date="20170805",num="167"},
+   ["Water"]={code="sm4",setName="Crimson Invasion",date="20171103",num="124"},
+   ["Lightning"]={code="sm2",setName="Guardians Rising",date="20170505",num="168"},
+   ["Psychic"]={code="sm1",setName="Sun & Moon",date="20170203",num="162"},
+   ["Fighting"]={code="sm2",setName="Guardians Rising",date="20170505",num="169"},
+   ["Darkness"]={code="sm3",setName="Burning Shadows",date="20170805",num="168"},
+   ["Metal"]={code="sm1",setName="Sun & Moon",date="20170203",num="163"},
+   ["Fairy"]={code="sm3",setName="Burning Shadows",date="20170805",num="169"}
+  }
+ },
+ {set="Team Up",
+  types={
+   ["Grass"]={steamUrl="1832405798628986164/8DAE81347A07897ACD085586A20D95CF2598E347/",num="1"},
+   ["Fire"]={steamUrl="1832405798628987149/3187D01AA63593BAD2A13058C9A64986771B83D5/",num="2"},
+   ["Water"]={steamUrl="1832405798628988741/04A35BDD099EBE7E18DBB9D7E7AB999D5BA66D9E/",num="3"},
+   ["Lightning"]={steamUrl="1832405798628989261/5967BCC4EB91584ABEB1680F1CF83B70302EAF52/",num="4"},
+   ["Psychic"]={steamUrl="1832405798628989709/D12DB2BE41A2185C11D87062EC08E285969F82A5/",num="5"},
+   ["Fighting"]={steamUrl="1832405798628991233/69EB109A4FE5D47D1B58FB59EC2517A30DB1DA20/",num="6"},
+   ["Darkness"]={steamUrl="1832405798628991928/8B177CCA848C1747B3790053958FAA79F3E0314B/",num="7"},
+   ["Metal"]={steamUrl="1832405798628992609/CC82A01EDC098ABEDA1376083E17D028F387A0A7/",num="8"},
+   ["Fairy"]={steamUrl="1832405798628993182/0CBE5BDF044B22B16510A8699F3B00CAC229C14C/",num="9"}
+  },
+  date="20190201"
+ },
+ {set="Sword & Shield",
+  types={
+   ["Grass"]={steamUrl="1832405798628838691/9F72772F0F21C4F6455D35CDDA7C0387D2C344CD/",num="1"},
+   ["Fire"]={steamUrl="1832405798628839998/882EB1317DC1D12FF4994221E57FE41B4ECF8D70/",num="2"},
+   ["Water"]={steamUrl="1832405798628840522/64FB79B3C680D7B11BAEFD367E03115243508AC2/",num="3"},
+   ["Lightning"]={steamUrl="1832405798628842568/67A8211D36AE2FF603C7A4B775C894EA25EA9155/",num="4"},
+   ["Psychic"]={steamUrl="1832405798628843075/407375AECC10AD4150F2C70FBB63A0BAC0A9AE05/",num="5"},
+   ["Fighting"]={steamUrl="1832405798628843991/B1DDC67C84E0E0E1527F85058DCDF038DB9D8663/",num="6"},
+   ["Darkness"]={steamUrl="1832405798628844608/70C62511CA9F2631D1E1DE691F4099A8102EE7EC/",num="7"},
+   ["Metal"]={steamUrl="1832405798628845093/EC66EEED31A9BAB8CCEDBB7152B0B4EC4C9D6F55/",num="8"},
+   ["Fairy"]={steamUrl="1832405798628845735/B64816866B1D0B67963CE2E9114F77A2E9C3BD2E/",num="9"}
+  },
+  date="20200207"
+ },
+ {set="Gen 8 Secret",
+  api=true,
+  types={
+   ["Grass"]={code="swsh8",setName="Fusion Strike",date="20211112",num="283"},
+   ["Fire"]={code="swsh8",setName="Fusion Strike",date="20211112",num="284"},
+   ["Water"]={code="swsh6",setName="Chilling Reign",date="20210618",num="231"},
+   ["Lightning"]={code="swsh7",setName="Evolving Skies",date="20210827",num="235"},
+   ["Psychic"]={code="swsh6",setName="Chilling Reign",date="20210618",num="232"},
+   ["Fighting"]={code="swsh6",setName="Chilling Reign",date="20210618",num="233"},
+   ["Darkness"]={code="swsh7",setName="Evolving Skies",date="20210827",num="236"},
+   ["Metal"]={code="swsh7",setName="Evolving Skies",date="20210827",num="237"}
+  }
+ }
+}
+
+function onLoad(state)
+ if state and state!=""then
+  local save=json.parse(state)
+  curSet=save.curSet
+  curType=save.curType
+ else
+  curSet=11
+  curType="Grass"
+ end
+ if self.GetCustomObject().diffuse!=getImageURL()then changeArt() return end
+ DrawUI()
+end
+
+function DrawUI()
+ local selfScale=self.getScale()
+ local iconScale=tostring(1/selfScale.x*0.75).." "..tostring(1/selfScale.z*0.75).." 1"
+ local count=1
+ local XMLTable={makeButtonUI("❮","prevSet()","170 180 0"),makeButtonUI("❯","nextSet()","-170 180 0")}
+
+ for Type,data in pairs(energyTable[curSet].types) do
+  local pos=""
+  if count<=3 then
+   pos=tostring(200-(100*count)).." -300 0"
+  else
+   pos=tostring(170+(-340*(count%2))).." "..tostring(-180+(120*math.floor((count-4)/2))).." 0"
+  end
+  XMLTable[#XMLTable+1]=makeTypeUI(Type,iconScale,pos)
+  count=count+1
+ end
+ self.UI.setXmlTable(XMLTable)
+end
+
+function makeTypeUI(Type,scale,pos)
+ return {
+  tag="Image",
+  attributes={
+   image=Type,
+   height=100,
+   width=100,
+   position=pos,
+   rotation="0 0 180",
+   scale=scale,
+   onClick="changeType("..Type..")",
+  }}
+end
+
+function makeButtonUI(text,func,pos)
+ return {
+  tag="Panel",
+  attributes={
+   height=90,
+   width=60,
+   scale=iconScale,
+   rotation="0 0 180",
+   position=pos,
+   color="rgba(0,0,0,0.7)",
+   onClick=func,
+  },
+  children={
+   tag="Text",
+   value=text,
+   attributes={
+    alignment="UpperCenter",
+    fontSize=70,
+    color="White",
+ }}}
+end
+
+function changeType(player,newType)
+ curType=newType
+ changeArt()
+end
+
+function prevSet()
+ curSet=curSet-1
+ if curSet==0 then curSet=#energyTable end
+ changeArt()
+end
+
+function nextSet()
+ curSet=curSet+1
+ if curSet>#energyTable then curSet=1 end
+ changeArt()
+end
+
+function onObjectLeaveContainer(cont,leaving)
+ if cont~=self then return end
+ local setTable=energyTable[curSet]
+ leaving.setCustomObject({face=getImageURL(),back="http://cloud-3.steamusercontent.com/ugc/809997459557414686/9ABD9158841F1167D295FD1295D7A597E03A7487/"})
+ leaving.setName(curType.." Energy")
+ leaving.setDescription((setTable.types[curType].setName or setTable.set).." #"..setTable.types[curType].num)
+ leaving.memo=((setTable.types[curType].date or setTable.date)..setTable.types[curType].num)
+end
+
+function getImageURL()
+ local setTable=energyTable[curSet]
+ if setTable.api then
+  return "https://images.pokemontcg.io/"..(setTable.code or setTable.types[curType].code).."/"..setTable.types[curType].num.."_hires.png?count="..setTable.types[curType].num
+ else
+  return "http://cloud-3.steamusercontent.com/ugc/"..setTable.types[curType].steamUrl
+ end
+end
+
+function changeArt()
+ setTable=energyTable[curSet]
+ if not setTable.types[curType] then
+  curType="Grass"
+ end
+ saveData()
+ self.setDescription(setTable.set.." "..curType.." Energy")
+ self.setCustomObject({diffuse=getImageURL()})
+ self.reload()
+end
+
+function saveData()
+  local save={curType=curType,curSet=curSet}
+  self.script_state=json.serialize(save)
+end
